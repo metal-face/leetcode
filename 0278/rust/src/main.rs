59ad9323299367c19569eeb6e56d80b24cd3504f
metal-face <hughesbryan3000@gmail.com>
2024-02-10 00:36:23 -0500

    solution reached

--- 0278/rust/src/main.rs
+++ 0278/rust/src/main.rs
@@ -1,3 +1,33 @@
+struct Solution;
+
+#[allow(dead_code)]
+impl Solution {
+   pub fn first_bad_version(&self, n: i32) -> i32 {
+        let mut left = 1;
+        let mut right = n;
+
+        while left < right {
+            let middle = left + (right - left) / 2;
+
+            if self.isBadVersion(middle) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+
+        }
+
+        return left;
+    } 
+
+
+    #[allow(non_snake_case)]
+    fn isBadVersion(&self, version: i32) -> bool {
+        println!("{}", version);
+        unimplemented!()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
 }
